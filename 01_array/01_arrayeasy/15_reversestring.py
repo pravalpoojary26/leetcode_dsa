c74169f38eaaf8eacaa6649ca4ec2cd17f3205f8
First_Project <pravalpoojary26@gmail.com>
2026-08-25 19:23:44 +0530

    Solve Reverse String and Best Time to Buy and Sell Stock

---
+++ 01_array/01_arrayeasy/15_reversestring.py
@@ -0,0 +1,19 @@
+
+'''
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+'''
+
+def reversestring(s):
+    left=0
+    right=len(s)-1
+
+    while left<right:
+        s[left],s[right]=s[right],s[left]
+
+        left+=1
+        right-=1
+
+    return s
+
