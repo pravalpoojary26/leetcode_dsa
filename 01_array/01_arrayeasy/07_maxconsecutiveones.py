5a9ab7bc13ffbe35d3283e4e700f31c39c8fb552
First_Project <pravalpoojary26@gmail.com>
2026-09-17 21:40:52 +0530

    Solved maxconsecutiveones

---
+++ 01_array/01_arrayeasy/07_maxconsecutiveones.py
@@ -0,0 +1,23 @@
+
+'''
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+'''
+
+def maxones(nums):
+    count=0
+    maxcount=0
+
+    for i in nums:
+        if i==0:
+            if count>maxcount:
+                maxcount=count
+                count=0
+        else:
+            count+=1
+
+    if count>maxcount:
+        maxcount=count
+
+    return maxcount
+
+print(maxones([0,1,1,1,0,1,1]))
