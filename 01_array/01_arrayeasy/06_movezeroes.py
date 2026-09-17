228f34a7e63822181940afc33dbce9296252e398
First_Project <pravalpoojary26@gmail.com>
2026-09-17 21:05:06 +0530

    Solved movezeroes

---
+++ 01_array/01_arrayeasy/06_movezeroes.py
@@ -0,0 +1,21 @@
+
+'''
+Given an integer array nums, move all 0's to the end of it while maintaining the relative
+order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+'''
+
+def movezeroes(nums):
+    j = 0
+
+    for i in range(len(nums)):
+        if nums[i]!=0:
+            nums[j],nums[i]=nums[i],nums[j]
+            j+=1
+
+    return nums
+
+print(movezeroes([0,1,0,0,2,3]))
+        
+
